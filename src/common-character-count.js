fb8163dbe392517b69ac07ab3bf2a00c636c1315
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-02-22 23:11:39 +0600

    feat: pass tests for common-character-count

--- src/common-character-count.js
+++ src/common-character-count.js
@@ -1,21 +1,17 @@
 const { NotImplementedError } = require("../extensions/index.js");
 
-/**
- * Given two strings, find the number of common characters between them.
- *
- * @param {String} s1
- * @param {String} s2
- * @return {Number}
- *
- * @example
- * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
- * Strings have 3 common characters - 2 "a"s and 1 "c".
- */
 function getCommonCharacterCount(s1, s2) {
-  let str = s1 + s2;
-  str = str.split("");
+  const s1Chars = s1.split("");
+  let commonCount = 0;
 
-  return new Set(str).size;
+  for (let i = 0; i < s1Chars.length; i++) {
+    if (s2.includes(s1Chars[i])) {
+      commonCount++;
+      s2 = s2.replace(s1Chars[i], "");
+    }
+  }
+
+  return commonCount;
 }
 
 module.exports = {
