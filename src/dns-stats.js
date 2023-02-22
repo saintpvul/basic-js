d53f26128b04f944157352c01a52f9ee95473d7e
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-02-22 23:13:29 +0600

    feat: pass tests for dns-stats

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -1,32 +1,20 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
-/**
- * Given an array of domains, return the object with the appearances of the DNS.
- *
- * @param {Array} domains
- * @return {Object}
- *
- * @example
- * domains = [
- *  'code.yandex.ru',
- *  'music.yandex.ru',
- *  'yandex.ru'
- * ]
- *
- * The result should be the following:
- * {
- *   '.ru': 3,
- *   '.ru.yandex': 3,
- *   '.ru.yandex.code': 1,
- *   '.ru.yandex.music': 1,
- * }
- *
- */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getDNSStats(domains) {
+  const stats = {};
+
+  domains.forEach((domain) => {
+    const parts = domain.split(".").reverse();
+    let subdomain = "";
+    parts.forEach((part) => {
+      subdomain += `.${part}`;
+      stats[subdomain] = (stats[subdomain] || 0) + 1;
+    });
+  });
+
+  return stats;
 }
 
 module.exports = {
-  getDNSStats
+  getDNSStats,
 };
