853ceb625992c097a66e205087d57442c788d3fd
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-02-22 21:50:27 +0600

    feat: pass tests for mine-sweeper

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -1,33 +1,35 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
+function minesweeper(matrix) {
+  const numRows = matrix.length;
+  const numCols = matrix[0].length;
+  const result = [];
 
-/**
- * In the popular Minesweeper game you have a board with some mines and those cells
- * that don't contain a mine have a number in it that indicates the total number of mines
- * in the neighboring cells. Starting off with some arrangement of mines
- * we want to create a Minesweeper game setup.
- *
- * @param {Array<Array>} matrix
- * @return {Array<Array>}
- *
- * @example
- * matrix = [
- *  [true, false, false],
- *  [false, true, false],
- *  [false, false, false]
- * ]
- *
- * The result should be following:
- * [
- *  [1, 2, 1],
- *  [2, 1, 1],
- *  [1, 1, 1]
- * ]
- */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+  for (let i = 0; i < numRows; i++) {
+    const row = [];
+    for (let j = 0; j < numCols; j++) {
+      let numMines = 0;
+      // check surrounding cells for mines
+      for (let x = Math.max(0, i - 1); x <= Math.min(numRows - 1, i + 1); x++) {
+        for (
+          let y = Math.max(0, j - 1);
+          y <= Math.min(numCols - 1, j + 1);
+          y++
+        ) {
+          if (x !== i || y !== j) {
+            if (matrix[x][y]) {
+              numMines++;
+            }
+          }
+        }
+      }
+      row.push(numMines);
+    }
+    result.push(row);
+  }
+
+  return result;
 }
 
 module.exports = {
-  minesweeper
+  minesweeper,
 };
