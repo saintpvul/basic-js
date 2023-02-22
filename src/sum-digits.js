fc70e01d0640de748e348eb4b3d5c264777c0c60
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-02-22 21:40:37 +0600

    feat: pass tests for sum-digits

--- src/sum-digits.js
+++ src/sum-digits.js
@@ -1,22 +1,18 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
-/**
- * Given a number, replace this number with
- * the sum of its digits until we get to a one digit number.
- *
- * @param {Number} n
- * @return {Number}
- *
- * @example
- * For 100, the result should be 1 (1 + 0 + 0 = 1)
- * For 91, the result should be 1 (9 + 1 = 10, 1 + 0 = 1)
- *
- */
-function getSumOfDigits(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getSumOfDigits(n) {
+  let curr = n;
+
+  while (curr.toString().length > 1) {
+    curr = curr
+      .toString()
+      .split("")
+      .reduce((s, v) => (s += +v), 0);
+  }
+
+  return curr;
 }
 
 module.exports = {
-  getSumOfDigits
+  getSumOfDigits,
 };
