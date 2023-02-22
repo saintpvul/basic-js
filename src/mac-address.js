6f7883046107c117eb795a85ae0e76670f1aff4f
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-02-22 23:01:47 +0600

    feat: pass tests for mac-address

--- src/mac-address.js
+++ src/mac-address.js
@@ -1,8 +1,8 @@
 const { NotImplementedError } = require("../extensions/index.js");
-
 function isMAC48Address(n) {
-  return n.split("-").filter((hex) => /[0-9A-Fa-f]{2}/g.test(hex)).length === 8;
+  return /^([0-9A-Fa-f]{2}-){5}[0-9A-Fa-f]{2}$/.test(n);
 }
+
 module.exports = {
   isMAC48Address,
 };
