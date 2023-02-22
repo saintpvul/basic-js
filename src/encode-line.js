1b450bc2488bb613b4f32145fded3d5cb2e603b0
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-02-22 23:00:05 +0600

    feat: pass tests for encode-line

--- src/encode-line.js
+++ src/encode-line.js
@@ -1,20 +1,21 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
-/**
- * Given a string, return its encoding version.
- *
- * @param {String} str
- * @return {String}
- *
- * @example
- * For aabbbc should return 2a3bc
- *
- */
-function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function encodeLine(str) {
+  let result = "";
+  let count = 1;
+  for (let i = 1; i <= str.length; i++) {
+    if (i === str.length || str[i] !== str[i - 1]) {
+      result += count > 1 ? count + str[i - 1] : str[i - 1];
+      count = 1;
+    } else {
+      count++;
+    }
+  }
+  return result;
 }
 
 module.exports = {
-  encodeLine
+  encodeLine,
 };
+
+console.log(encodeLine("aabbbc"));
--- src/mac-address.js
+++ src/mac-address.js
@@ -1,23 +1,8 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
-/**
- * The MAC-48 address is six groups of two hexadecimal digits (0 to 9 or A to F),
- * separated by hyphens.
- *
- * Your task is to check by given string inputString
- * whether it's a MAC-48 address or not.
- *
- * @param {Number} inputString
- * @return {Boolean}
- *
- * @example
- * For 00-1B-63-84-45-E6, the output should be true.
- *
- */
-function isMAC48Address(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function isMAC48Address(n) {
+  return n.split("-").filter((hex) => /[0-9A-Fa-f]{2}/g.test(hex)).length === 8;
 }
 module.exports = {
-  isMAC48Address
+  isMAC48Address,
 };
